452078c7a9726299614aab4e0d805191412e7053
casaLiu <jh16procasa@gmail.com>
2014-09-11 08:28:52 +0800

    modify

--- Services/VoiceRecordSyncServices/VoiceRecordEnum.py
+++ Services/VoiceRecordSyncServices/VoiceRecordEnum.py
@@ -7,3 +7,24 @@ from richenum import RichEnum
 from richenum import RichEnumValue
 
 
+class EncodingEnumValue(RichEnumValue):
+    def __init__(self, flavor, *args):
+        super(EncodingEnumValue, self).__init__(*args)
+        self.flavor = flavor
+
+
+class AudioFormatEnumValue(RichEnum):
+    def __init__(self, flavor, *args):
+        super(AudioFormatEnumValue, self).__init__(*args)
+        self.flavor = flavor
+
+
+class ChannelEnumValue(RichEnum):
+    def __init__(self, flavor, *args):
+        super(ChannelEnumValue, self).__init__(*args)
+        self.flavor = flavor
+
+
+
+
+
